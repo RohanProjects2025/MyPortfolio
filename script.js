3f89b16e171e91947e685c19342022d1cb58ed43
RohanProjects2025 <rohanvmadane@gmail.com>
2026-05-30 07:53:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,35 @@ toggleBtn.addEventListener("click", () => {
     icon.classList.add("fa-moon");
   }
 });
+
+
+// Typing Animation
+
+const roles = [
+  "JAVA FULL STACK DEVELOPER",
+  "FRONTEND DEVELOPER",
+  "BACKEND DEVELOPER",
+  "PROBLEM SOLVER"
+];
+
+let roleIndex = 0;
+let charIndex = 0;
+
+const typing = document.getElementById("typing");
+
+function typeEffect() {
+  const currentRole = roles[roleIndex];
+
+  typing.textContent = currentRole.slice(0, charIndex +1);
+
+  charIndex++;
+
+  if(charIndex > currentRole.length){
+    
+    charIndex = 0;
+    
+    roleIndex = (roleIndex + 1) % roles.length;
+  }
+}
+
+setInterval(typeEffect, 150);
